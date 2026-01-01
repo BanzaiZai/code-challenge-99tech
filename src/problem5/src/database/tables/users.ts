1e44d569ca375eaacf8e18f191529a06122e57d6
Kevin Wong <kevin.wot@protonmail.com>
2026-01-01 15:18:06 +0800

    feat: generate initial Drizzle ORM migration for users table

---
+++ src/problem5/src/database/tables/users.ts
@@ -0,0 +1,7 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar("name").notNull(),
+  email: varchar("email").notNull().unique(),
+});
